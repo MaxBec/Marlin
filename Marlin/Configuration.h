ee1ae29641bb137f41cd709710a528eccae084b9
Max Beckenbauer <max.bec92@gmail.com>
2020-11-02 11:59:13 +0100

    Changed ABL probing times
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1066,8 +1066,8 @@
  * A total of 2 does fast/slow probes with a weighted average.
  * A total of 3 or more adds more slow probes, taking the average.
  */
-#define MULTIPLE_PROBING 2
-#define EXTRA_PROBING 4
+#define MULTIPLE_PROBING 6
+#define EXTRA_PROBING 1
 
 /**
  * Z probes require clearance when deploying, stowing, and moving between
@@ -1337,7 +1337,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
 // Set the number of grid points per dimension.
-#define GRID_MAX_POINTS_X 5
+#define GRID_MAX_POINTS_X 6
 #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
 // Probe along the Y axis, advancing X after each column
