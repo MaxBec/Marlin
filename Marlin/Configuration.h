2e3b83ae974953a8e9d81faa2a5e2ee97ca6b1c9
Max Beckenbauer <max.bec92@gmail.com>
2020-11-20 16:25:50 +0100

    Updates
    Signed-off-by: Max Beckenbauer <max.bec92@gmail.com>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1067,7 +1067,7 @@
  */
 #define NOZZLE_TO_PROBE_OFFSET \
     {                         \
-        28, -33, -2.41       \
+        28, -33, -2.40       \
     }
 
 // Most probes should stay away from the edges of the bed, but
@@ -1209,7 +1209,7 @@
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS -5
+#define Y_MIN_POS -8
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
